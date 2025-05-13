e467ae5fc08f75de0bfc4ac56055e0fcaecb785a
TruongManhHoang <truongmanhhoang481@gmail.com>
2025-05-13 16:56:34 +0700

    department

---
+++ lib/data/model/department/department_model.dart
@@ -0,0 +1,77 @@
+import 'package:hive/hive.dart';
+
+part 'department_model.g.dart';
+
+@HiveType(typeId: 2)
+class DepartmentModel {
+  @HiveField(0)
+  final String? id;
+  @HiveField(1)
+  final String name;
+  @HiveField(2)
+  final String? description;
+  @HiveField(3)
+  final String? managerName;
+  @HiveField(4)
+  final DateTime? createdAt;
+  @HiveField(5)
+  final DateTime? updatedAt;
+  @HiveField(6)
+  final String status;
+  @HiveField(7)
+  final int employeeCount;
+  @HiveField(8)
+  final String? code;
+  @HiveField(9)
+  final String? email;
+  @HiveField(10)
+  final String? phoneNumber;
+
+  DepartmentModel({
+    this.id,
+    required this.name,
+    this.description,
+    this.managerName,
+    this.createdAt,
+    this.updatedAt,
+    required this.status,
+    required this.employeeCount,
+    this.code,
+    this.email,
+    this.phoneNumber,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'managerName': managerName,
+      'createdAt': createdAt?.toIso8601String(),
+      'updatedAt': updatedAt?.toIso8601String(),
+      'status': status,
+      'employeeCount': employeeCount,
+      'code': code,
+      'email': email,
+      'phoneNumber': phoneNumber,
+    };
+  }
+
+  factory DepartmentModel.fromMap(Map<String, dynamic> map) {
+    return DepartmentModel(
+      id: map['id'],
+      name: map['name'],
+      description: map['description'],
+      managerName: map['managerName'],
+      createdAt:
+          map['createdAt'] != null ? DateTime.parse(map['createdAt']) : null,
+      updatedAt:
+          map['updatedAt'] != null ? DateTime.parse(map['updatedAt']) : null,
+      status: map['status'],
+      employeeCount: map['employeeCount'],
+      code: map['code'],
+      email: map['email'],
+      phoneNumber: map['phoneNumber'],
+    );
+  }
+}
---
+++ lib/service/department_service.dart
@@ -0,0 +1,32 @@
+import 'package:admin_hrm/data/model/department/department_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DepartmentService {
+  final _firestore = FirebaseFirestore.instance;
+
+  Future<void> addDepartment(DepartmentModel departmentModel) async {
+    await _firestore
+        .collection('departments')
+        .doc(departmentModel.id)
+        .set(departmentModel.toMap());
+  }
+
+  Future<List<DepartmentModel>> getDepartments() async {
+    final snapshot = await _firestore.collection('departments').get();
+
+    return snapshot.docs
+        .map((doc) => DepartmentModel.fromMap(doc.data()))
+        .toList();
+  }
+
+  Future<void> updateDepartment(DepartmentModel department) async {
+    await _firestore
+        .collection('departments')
+        .doc(department.id)
+        .update(department.toMap());
+  }
+
+  Future<void> deleteDepartment(String id) async {
+    await _firestore.collection('departments').doc(id).delete();
+  }
+}
